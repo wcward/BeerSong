8427d19c2d3cb9535546bdeddf2fb33bd6759d86
Willam Ward <wcward@oakland.edu>
2016-09-30 21:31:54 -0400

    Added new method to BeerSongImplementation

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package beersong;
+
+
 
 /**
  *
- * @author codyw
+ * William Ward
+ * CIT 337
+ * 9/30/2016
  */
 public class BeerSong {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int beerNum = 99;
+        String word = "bottles";
+        while(beerNum > 0){
+            if (beerNum == 1){
+                word = "bottle";
+            }
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
+            
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+                //System.out.println("***************************");
+            }else {
+                System.out.println("No more bottles of beer on the wall");
+            }
+        }
     }
-    
 }
