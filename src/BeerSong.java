dc8459b3bc14e1575fd99d846dc7a3c09d901d99
Willam Ward <wcward@oakland.edu>
2016-09-30 21:42:18 -0400

    Pulled changes from the repo

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -11,22 +11,29 @@ public class BeerSong {
 
 
     public static void main(String[] args) {
-        int beerNum = 99;
-        String word = "bottles";
-        while(beerNum > 0){
-            if (beerNum == 1){
-                word = "bottle";
+        int beerNum = 99;                           //int to store Number of Bottle            
+        String word = "bottles";                    //Create String of Bottles
+        while(beerNum > 0){                         //While loop to count bottles
+            if (beerNum == 1){                      //Increment loop by 1
+                word = "bottle";                    //Word to use after number
             }
+            //Print number of bottles of beer on the wall
             System.out.println(beerNum + " " + word + " of beer on the wall");
+            //Print number of bottles of beer
             System.out.println(beerNum + " " + word + " of beer.");
+            //Print take one down
             System.out.println("Take one down.");
+            //Print pass it around
             System.out.println("Pass it around.");
+            //print Number of bottles of beer on the wall incremented by -1
             beerNum = beerNum - 1;
             
-            if (beerNum > 0) {
+            if (beerNum > 0) {                      //If there is more than 0 bottles
+                //Print the number of bottles of beer on the wall
                 System.out.println(beerNum + " " + word + " of beer on the wall");
-                //System.out.println("***************************");
+               //If there are 0 bottles of beer left
             }else {
+                //Print this
                 System.out.println("No more bottles of beer on the wall");
             }
         }
